ea01fb83c146a045114f28bfa30f1adfe6c1a8d7
hatsnake <hatsnake@naver.com>
2021-05-20 09:49:08 +0900

    folder structure & component 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
-//react hooks로 인해 class형태에서 function형태로 바뀜
+//컴포넌트 : react hooks로 인해 class형태에서 function형태로 바뀜
+
+//클래스 컴포넌트
+/*
+class App extends React.Component {
+  render() {
+    return <div className="App">Kossie Coder</div>;
+  }
+}
+*/
+
+//함수 컴포넌트
 function App() {
   return (
     // JSX : JS를 확장한 문법
