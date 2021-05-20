908c6b348f37bb616fb8b267c8f1554b80265d1d
hatsnake <hatsnake@naver.com>
2021-05-20 10:27:04 +0900

    form useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 
+// https://www.youtube.com/watch?v=UFtirvA6NDU&list=PLB7CpjPWqHOuf62H44TMkMIsqfkIzcEcX&index=5
+
 //컴포넌트 : react hooks로 인해 class형태에서 function형태로 바뀜
 
 //클래스 컴포넌트
@@ -13,39 +15,27 @@ class App extends React.Component {
 
 //함수 컴포넌트
 function App() {
-  const [text, setText] = useState('Kossie');
-  const onSubmit = () => {
-    alert('submitted');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(username, password);
   }
-
-  const onKeyUp = (event) => {
-    if(event.keyCode === 13) {
-      onSubmit();
-    }
-  }
-
-  //let text = "Kossie";
-
-  const updateText = () => {
-    //text = "Coder";
-    setText('Coder');
-    console.log(text);
-  }
-
-  console.log(text);
 
   return (
     // JSX : JS를 확장한 문법
     // onClick={함수}
     // JS Event를 카멜 표기법으로 사용가능
     <div className="App">
-      <input onKeyUp={onKeyUp}/>
-      <button onClick={onSubmit}>Submit</button> 
-
-      <br/><br/>
-
-      <span>{text}</span>
-      <button onClick={updateText}>Update</button>
+      <form onSubmit={onSubmit}>
+        <input type="text" placeholder="Username" 
+          value={username} onChange={(e) => setUsername(e.target.value)} />
+        <br/>
+        <input type="password" placeholder="Password" 
+          value={password} onChange={(e) => setPassword(e.target.value)} />
+        <br/>
+        <button type="submit">Login</button> 
+      </form>
     </div>
   );
 }
