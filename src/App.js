9da490b9fb42c8645e8e68c0300cb237065fa11d
hatsnake <hatsnake@naver.com>
2021-05-21 09:58:08 +0900

    Eliminate iterations with components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
+import Counter from './components/Counter';
 
 // https://www.youtube.com/watch?v=UFtirvA6NDU&list=PLB7CpjPWqHOuf62H44TMkMIsqfkIzcEcX&index=5
 
@@ -15,26 +16,12 @@ class App extends React.Component {
 
 //함수 컴포넌트
 function App() {
-  const [count, setCount] = useState(0);
-  const [kossie, setKossie] = useState(0);
-  useEffect(() => {
-    console.log(count);
-  }, [count, kossie]);
-  
-  //한번만 실행
-  useEffect(() => {
-    console.log('first rendering');
-  }, []);
-
-  const increment = () => {
-    setCount(count + 1);
-  };
-
   return (
     <div className="App">
       <h1>Kossie Coder</h1>
-      <button onClick={increment}>Click</button>
-      <button onClick={() => setKossie(kossie + 1)}>Click</button>      
+      <Counter />
+      <Counter />
+      <Counter />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 
 // ReactDOM.render(element, container[, callback])
 ReactDOM.render(
-  //StrictMode : 개발과정에서 도와주는 역할, 없어도 됨.
   <App />,
   document.getElementById('root')
 );
