bdd3f7ce5c686ff436b88d4f26e2c019bfa631f3
hatsnake <hatsnake@naver.com>
2021-05-29 16:08:21 +0900

    Son Component -> Parents Component Change State

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import Movie from "./components/Movie";
+import MovieForm from './components/MovieForm';
 
 // https://www.youtube.com/watch?v=UFtirvA6NDU&list=PLB7CpjPWqHOuf62H44TMkMIsqfkIzcEcX&index=5
 
@@ -16,18 +17,12 @@ class App extends React.Component {
 
 //함수 컴포넌트
 function App() {
-  const [movieTitle, setMovieTitle] = useState('');
-  const [movieYear, setMovieYear] = useState('');
   const [movies, setMovies] = useState([
     { title: 'Kossie coder1', year: 2001 },
     { title: 'Kossie coder2', year: 2002 },
     { title: 'Kossie coder3', year: 2003 },
     { title: 'Kossie coder4', year: 2004 },
   ]);
-  
-  useEffect(() => {
-    //console.log('render');
-  });
 
   const renderMovies = movies.map(movie => {
     //JSX형태로 리턴
@@ -36,34 +31,18 @@ function App() {
     );
   });
   
-  const addMovie = (event) => {
-    event.preventDefault();
-
+  const addMovie = (movie) => {
     setMovies([
       //...movies : 기존배열이 덮어지는 것을 방지
       ...movies,
-      {
-        title: movieTitle,
-        year: movieYear,
-      }
+      movie
     ]);
-
-    setMovieTitle('');
-    setMovieYear('');
   };
 
   return (
     <div className="App">
       <h1>Movie list</h1>
-      <form onSubmit={addMovie}>
-        <input type="text" value={movieTitle} placeholder="영화제목" 
-              onChange={e => setMovieTitle(e.target.value)}/>
-        <br/>
-        <input type="text" value={movieYear} placeholder="개봉년도" 
-              onChange={e => setMovieYear(e.target.value)}/>
-        <br/>
-        <button type="submit">영화추가</button>
-      </form>
+      <MovieForm addMovie={addMovie} />
       {renderMovies}
     </div>
   );
